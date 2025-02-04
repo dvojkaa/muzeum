dc8e995f879b19f45b6a22153e1a309e4023c1f6
vojtaPC <krativoj@fel.cvut.cz>
2025-02-04 15:59:16 +0100

    Dodelavani modelu Art a pridani DBS modelu

--- BackEnd/src/main/java/cz/cvut/fel/MuzeumSys/model/Art.java
+++ BackEnd/src/main/java/cz/cvut/fel/MuzeumSys/model/Art.java
@@ -12,18 +12,40 @@ import lombok.Setter;
 @Setter
 @Entity(name = "art")
 @NamedQueries({
-        @NamedQuery(name = "Art.findByname", query = "SELECT a FROM art a WHERE a.name = :name")
+        @NamedQuery(name = "Art.findByName", query = "SELECT a FROM art a WHERE a.name = :name"),
+        @NamedQuery(name = "Art.findByEra", query = "SELECT a FROM art a WHERE a.era = :era"),
+        @NamedQuery(name = "Art.findByType", query = "SELECT a FROM art a WHERE a.type = :type"),
+        @NamedQuery(name = "Art.findByAuthor", query = "SELECT a FROM art a WHERE a.author = :author")
 })
 public class Art extends AbstractEntity {
+
     @Basic(optional = false)
     @Column(nullable = false)
-    private String email;
     private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Basic(optional = false)
+    @Column(nullable = false)
     private Era era;
+
+    @Basic(optional = false)
+    @Column(nullable = false)
     private Type type;
+
+    @Basic(optional = false)
+    @Column(nullable = false)
     private Double qrCode;
+
+    @Basic(optional = false)
+    @Column(nullable = false)
     private String discription;
+
+    @Basic(optional = false)
+    @Column(nullable = false)
     private String parametrs;
+
+    @Basic(optional = false)
+    @Column(nullable = false)
     private String author;
 
 }
