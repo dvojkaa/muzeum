f4be57790c28a436467de5d8b64e2bd81326ce25
Vojtěch Kratina <krativoj@fel.cvut.cz>
2025-02-12 15:45:03 +0100

    Dodělávání modelů

--- BackEnd/src/main/java/cz/cvut/fel/MuzeumSys/model/Admin.java
+++ BackEnd/src/main/java/cz/cvut/fel/MuzeumSys/model/Admin.java
@@ -1,9 +1,7 @@
 package cz.cvut.fel.MuzeumSys.model;
 
 import cz.cvut.fel.nss.SaunaStudio.model.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.NamedQueries;
-import jakarta.persistence.NamedQuery;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,5 +14,13 @@ import lombok.Setter;
         @NamedQuery(name = "Admin.findByUsername", query = "SELECT a FROM admins a WHERE a.email = :email")
 })
 public class Admin extends User {
+
+    @Column(nullable = false)
+    @Basic(optional = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    @Basic(optional = false)
+    private String lastName;
 
 }
--- BackEnd/src/main/java/cz/cvut/fel/MuzeumSys/model/Art.java
+++ BackEnd/src/main/java/cz/cvut/fel/MuzeumSys/model/Art.java
@@ -1,21 +1,22 @@
 package cz.cvut.fel.MuzeumSys.model;
 
-
-import cz.cvut.fel.nss.SaunaStudio.model.AbstractEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @Getter
 @Setter
-@Entity(name = "art")
+@ToString
+@Entity
+@Table(name = "art")
 @NamedQueries({
-        @NamedQuery(name = "Art.findByName", query = "SELECT a FROM art a WHERE a.name = :name"),
-        @NamedQuery(name = "Art.findByEra", query = "SELECT a FROM art a WHERE a.era = :era"),
-        @NamedQuery(name = "Art.findByType", query = "SELECT a FROM art a WHERE a.type = :type"),
-        @NamedQuery(name = "Art.findByAuthor", query = "SELECT a FROM art a WHERE a.author = :author")
+        @NamedQuery(name = "Art.findByName", query = "SELECT a FROM Art a WHERE a.name = :name"),
+        @NamedQuery(name = "Art.findByEra", query = "SELECT a FROM Art a WHERE a.era = :era"),
+        @NamedQuery(name = "Art.findByType", query = "SELECT a FROM Art a WHERE a.type = :type"),
+        @NamedQuery(name = "Art.findByAuthor", query = "SELECT a FROM Art a WHERE a.author = :author")
 })
 public class Art extends AbstractEntity {
 
@@ -28,24 +29,32 @@ public class Art extends AbstractEntity {
     @Column(nullable = false)
     private Era era;
 
+    @Enumerated(EnumType.STRING)
     @Basic(optional = false)
     @Column(nullable = false)
     private Type type;
 
     @Basic(optional = false)
     @Column(nullable = false)
-    private Double qrCode;
+    private String qrCode;
 
     @Basic(optional = false)
     @Column(nullable = false)
-    private String discription;
+    private String description;
 
     @Basic(optional = false)
     @Column(nullable = false)
-    private String parametrs;
+    private String parameters;
 
     @Basic(optional = false)
     @Column(nullable = false)
     private String author;
 
+    @ManyToOne
+    @JoinColumn(name = "group_id", nullable = false)
+    private Group group;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
 }
