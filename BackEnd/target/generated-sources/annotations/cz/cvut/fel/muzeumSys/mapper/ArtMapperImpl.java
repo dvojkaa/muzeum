33c45b54002d4e57e8410aeb76d025c61f4c826b
Vojtěch Kratina <krativoj@fel.cvut.cz>
2025-04-23 17:45:05 +0200

    Deploy

--- BackEnd/target/generated-sources/annotations/cz/cvut/fel/muzeumSys/mapper/ArtMapperImpl.java
+++ BackEnd/target/generated-sources/annotations/cz/cvut/fel/muzeumSys/mapper/ArtMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-23T17:21:40+0200",
+    date = "2025-04-23T17:39:49+0200",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
 )
 @Component
